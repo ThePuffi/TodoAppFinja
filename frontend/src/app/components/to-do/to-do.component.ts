8bd5fdc8f1a8e39660bd96c358e1dbb08f29577b
0oru <ooru@proton.me>
2024-05-22 16:36:52 +0200

    worked on todo table

--- frontend/src/app/components/to-do/to-do.component.ts
+++ frontend/src/app/components/to-do/to-do.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { ToDo } from '../../models/to-do';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-to-do',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './to-do.component.html',
   styleUrl: './to-do.component.scss'
 })
 export class ToDoComponent {
+  
+  constructor() { }
+
+  protected columns = [
+    { key: 'name', label: 'Title'},
+    { key: 'dueDate', label: 'Due Date'}
+  ];
+
+  protected displayedTodoData: ToDo[] = [
+    {
+      id: 1,
+      name: "Projekt erstellen",
+      status: false,
+      categoryId: 1,
+      dueDate: new Date(),
+      description: "Description",
+      members: [
+        {
+          id: 1,
+          username: "ole_w",
+          firstName: "Ole",
+          lastName: "W",
+          password: "1234",
+          email: "ole@mail.de",
+        }
+      ]
+    }
+  ];
+
+  addTodo() {
+
+  }
 
 }
